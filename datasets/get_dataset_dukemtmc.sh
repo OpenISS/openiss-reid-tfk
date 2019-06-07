86928854da379b2506369d3982a578c94eedbb2d
UNO998 <shenyiran2015@outlook.com>
2019-06-07 15:44:46 +0200

    fix the dataset script

--- datasets/get_dataset_dukemtmc.sh
+++ datasets/get_dataset_dukemtmc.sh
@@ -1,6 +1,6 @@
 echo 'Please enter your user name for bitbucket'
 read username
-wget https://drive.google.com/uc?id=0B0VOCNYh8HeRdnBPa2ZWaVBYSVk
+wget --user=$username --ask-password https://drive.google.com/uc?id=0B0VOCNYh8HeRdnBPa2ZWaVBYSVk
 mv DukeMTMC-reID.zip dukemtmc-reid.zip
 unzip dukemtmc-reid.zip
 rm -rf dukemtmc-reid.zip
--- datasets/get_dataset_market1501.sh
+++ datasets/get_dataset_market1501.sh
@@ -1,6 +1,6 @@
 echo 'Please enter your user name for bitbucket'
 read username
-wget https://drive.google.com/file/d/0B8-rUzbwVRk0c054eEozWG9COHM/view
+wget --user=$username --ask-password https://bitbucket.org/eric_thesis/reid-tfk/downloads/market1501.zip
 mv Market-1501-v15.09.15.zip market1501.zip
 unzip market1501.zip
 rm -rf market1501.zip
