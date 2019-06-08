f6a643577444131a28553ce3e9e05cdbf01ae89e
UNO998 <shenyiran2015@outlook.com>
2019-06-08 15:38:36 +0200

    fix dataset script

--- datasets/get_dataset_market1501.sh
+++ datasets/get_dataset_market1501.sh
@@ -4,3 +4,4 @@ wget --user=$username --ask-password https://bitbucket.org/eric_thesis/reid-tfk/
 mv Market-1501-v15.09.15.zip market1501.zip
 unzip market1501.zip
 rm -rf market1501.zip
+mv Market-1501-v15.09.15 market1501
